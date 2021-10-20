fdf1a16a0d296a0e1ccc2eaa2697bffb43bdf524
Douglas Henrique Alves <doughalves@gmail.com>
2021-10-20 12:48:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var numeroSecreto = parseInt(Math.random() * 11);
+console.log(numeroSecreto);
+
+function Chutar() {
+  var elementoResultado = document.getElementById("resultado");
+  var chute = parseInt(document.getElementById("valor").value);
+  console.log(chute);
+  if (chute == numeroSecreto) {
+    elementoResultado.innerHTML = "Você acertou";
+  } else if (chute > 10 || chute < 0) {
+    elementoResultado.innerHTML = "Você deve escolher um número de 0 a 10";
+  } else if (chute > numeroSecreto) {
+    elementoResultado.innerHTML = "Quase!!! o numero secreto é menor";
+  } else {
+    elementoResultado.innerHTML = "Quase!!! o numero secreto é maior";
+  }
+}
